15493d4fc230760ee67070c3e933b3ba7c9a1e39
Prabhjot Singh Rai <prabhjotsinghrai1@gmail.com>
2022-12-08 22:41:26 +0530

    feat: add login and register buttons to app bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,17 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useLocation, useNavigate } from 'react-router-dom'
+import { setToken } from "../features/user/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function Header() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -23,6 +30,12 @@ export default function Header() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         BetaCraft Demo
                     </Typography>
+                    { location.pathname === '/dashboard' ? <Button onClick={() => {
+                        dispatch(setToken(null))
+                        navigate('/login')
+                    }} color="inherit">Logout</Button> : null }
+                    { location.pathname === '/' || location.pathname === '/register' ? <Button onClick={() => navigate('/login')} color="inherit">Login</Button> : null }
+                    { location.pathname === '/' || location.pathname === '/login' ? <Button onClick={() => navigate('/register')} color="inherit">Register</Button> : null }
                 </Toolbar>
             </AppBar>
         </Box>
